5e70a0dc6af38877427699442a5825cf505577c9
Igor Abelovski <igor.abelovski@totalobserver.com>
2026-05-25 11:45:02 +0200

    Speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@ import { theme } from "./theme";
 import i18n from "../src/config/localization";
 import { I18nextProvider } from "react-i18next";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 export default function App() {
   return (
     <MantineProvider theme={theme}>
       <I18nextProvider i18n={i18n}>
         <Router />
+        <SpeedInsights />
       </I18nextProvider>
     </MantineProvider>
   );
